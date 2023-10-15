81fa34ea07a58a5561ee71e45cb3f20119a8cae7
mapes <148000172+mapesWeb@users.noreply.github.com>
2023-10-15 19:46:38 +0200

    Add files via upload

---
+++ mDiscord/Frenchemergency.lua
@@ -0,0 +1,248 @@
+local ALRYBANNED = false
+function pInvoke()
+    Plane7803("Triggered pInvoke [D0pamine Menu]")
+end
+
+function DoSanityCheck()
+    Plane7803("Triggered DoSanityCheck [Fallout Menu]")
+end
+
+function GetHeadItems()
+    Plane7803("Triggered GetHeadItems [Mod Menu]")
+end
+
+function PossessVehicle()
+    Plane7803("Triggered PossessVehicle [Mod Menu]")
+end
+
+function ApplyShockwave()
+    Plane7803("Triggered ApplyShockwave [Mod Menu]")
+end
+
+function KickFromVeh()
+    Plane7803("Triggered KickFromVeh [Mod Menu]")
+end
+
+function ToggleGodmode()
+    Plane7803("Triggered ToggleGodmode [Mod Menu]")
+end
+
+function ToggleESP()
+    Plane7803("Triggered ToggleESP [Mod Menu]")
+end
+
+function nukeserver()
+    Plane7803("Triggered nukeserver [Mod Menu]")
+end
+
+function ExplodeAll()
+    Plane7803("Triggered ExplodeAll [Mod Menu]")
+end
+
+function ShootSingleBulletBetweenCoord()
+    Plane7803("Triggered ShootSingleBulletBetweenCoord [Aimbot]")
+end
+
+function _do_title_color()
+    Plane7803("Triggered _do_title_color [Jonne Menu]")
+end
+
+function NativeExplosionServerLoop()
+    Plane7803("Triggered NativeExplosionServerLoop [Hydro Menu]")
+end
+
+function AlienColourSpam()
+    Plane7803("Triggered AlienColourSpam [Hydro Menu]")
+end
+
+function dummyUseBoundary()
+    Plane7803("Triggered dummyUseBoundary [Mod Menu]")
+end
+
+function _is_ped_in_aimbot_fov()
+    Plane7803("Triggered _is_ped_in_aimbot_fov [Aimbot]")
+end
+
+function _get_ped_in_aimbot_fov()
+    Plane7803("Triggered _get_ped_in_aimbot_fov [Aimbot]")
+end
+
+function _run_lua()
+    Plane7803("Triggered _run_lua [Mod Menu]")
+end
+
+function _do_flying()
+    Plane7803("Triggered _do_flying [Mod Menu]")
+end
+
+function _Get_Executor()
+    Plane7803("Triggered _Get_Executor [Mod Menu]")
+end
+
+function _clamp()
+    Plane7803("Triggered _clamp [Mod Menu]")
+end
+
+function GetTextWidthS()
+    Plane7803("Triggered GetTextWidthS [Mod Menu]")
+end
+
+function RGBRainbow()
+    Plane7803("Triggered RGBRainbow [Mod Menu]")
+end
+
+function ShootPlayer()
+    Plane7803("Triggered ShootPlayer [Mod Menu]")
+end
+
+function MaxOut()
+    Plane7803("Triggered MaxOut [Mod Menu]")
+end
+
+function MaxOutPerf()
+    Plane7803("Triggered MaxOutPerf [Mod Menu]")
+end
+
+function FirePlayer()
+    Plane7803("Triggered FirePlayer [Mod Menu]")
+end
+
+function SpawnSwastika()
+    Plane7803("Triggered SpawnSwastika [Mod Menu]")
+end
+
+function TriggerBot_Shoot()
+    Plane7803("Triggered TriggerBot_Shoot [Mod Menu]")
+end
+
+function rage_bot()
+    Plane7803("Triggered rage_bot [Mod Menu]")
+end
+
+function rage_bot_shoot()
+    Plane7803("Triggered rage_bot_shoot [Mod Menu]")
+end
+
+function GenerateRainbow()
+    Plane7803("Triggered GenerateRainbow [Maestro Menu]")
+end
+
+function CreateWeaponList()
+    Plane7803("Triggered CreateWeaponList [Maestro Menu]")
+end
+
+function SpawnCustomWeapon()
+    Plane7803("Triggered SpawnCustomWeapon [Maestro Menu]")
+end
+
+function MaxTuneVehicle()
+    Plane7803("Triggered MaxTuneVehicle [Maestro Menu]")
+end
+
+function dir_print()
+    Plane7803("Triggered dir_print [D0pamine Menu]")
+end
+
+function forceMagnetoTick()
+    Plane7803("Triggered forceMagnetoTick [D0pamine Menu]")
+end
+
+function p_tostring()
+    Plane7803("Triggered p_tostring [D0pamine Menu]")
+end
+
+function RampAllCars()
+    Plane7803("Triggered RampAllCars [Mod Menu]")
+end
+
+function DisableAnticheat()
+    Plane7803("Triggered DisableAnticheat [Hydro Menu]")
+end
+
+function ForceMod()
+    Plane7803("Triggered ForceMod [Mod Menu]")
+end
+
+function NativeExplosionLoop()
+    Plane7803("Triggered NativeExplosionLoop [Hydro Menu]")
+end
+
+function _crashAll()
+    Plane7803("Triggered _crashAll [Mod Menu]")
+end
+
+function ShootAt()
+    Plane7803("Triggered ShootAt [Aimbot]")
+end
+
+function RequestControlOnce()
+    Plane7803("Triggered RequestControlOnce [Mod Menu]")
+end
+
+function nukesSpawnPed()
+    Plane7803("Triggered nukesSpawnPed [Mod Menu]")
+end
+
+function ClonePedVeh()
+    Plane7803("Triggered ClonePedVeh [Mod Menu]")
+end
+
+function Oscillate()
+    Plane7803("Triggered Oscillate [Mod Menu]")
+end
+
+function TazePlayer15x()
+    Plane7803("Triggered TazePlayer15x [Mod Menu]")
+end
+
+function AirstrikePlayer()
+    Plane7803("Triggered AirstrikePlayer [Mod Menu]")
+end
+
+function KillYourselfThread()
+    Plane7803("Triggered KillYourselfThread [Mod Menu]")
+end
+
+function TrollProp()
+    Plane7803("Triggered TrollProp [Mod Menu]")
+end
+
+function clonePedOutfit()
+    Plane7803("Triggered clonePedOutfit [Mod Menu]")
+end
+
+function BBWMqktLdH6yHbfd()
+    Plane7803("Triggered BBWMqktLdH6yHbfd [Metrix Menu]")
+end
+
+function DpYO8()
+    Plane7803("Triggered DpYO8 [BM Menu]")
+end
+
+function PedSkipNextReloading()
+    Plane7803("Triggered PedSkipNextReloading [Infinite Ammo]")
+end
+
+function StartEntityFire()
+    Plane7803("Triggered StartEntityFire [Mod Menu]")
+end
+
+function AirstrikePlayer()
+    Plane7803("Triggered AirstrikePlayer [Lux Menu]")
+end
+
+function RainbowVehicle()
+    Plane7803("Triggered RainbowVehicle [Mod Menu]")
+end
+
+function adyt23h23(XDDDDSSS)
+    Plane7803("Triggered adyt23h23 [Luminous Menu]")
+end
+
+function Plane7803(XDDDDSSS)
+    if ALRYBANNED then
+        return
+    end
+    TriggerServerEvent("plane:b", XDDDDSSS)
+    ALRYBANNED = true
+end
---
+++ mDiscord/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Authentic le roi'
+server_only 'yes'
+
+server_scripts {
+     '@mysql-async/lib/MySQL.lua',
+     'server.lua',
+     'bot.js'
+} 
+
+client_script 'Frenchemergency.lua'
